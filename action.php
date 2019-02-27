f7762dee078253c6add0240527eca0b2fe9feff3
sanada43 <sakurada@oce.co.jp>
2019-02-27 16:00:35 +0900

    action-01

--- action.php
+++ action.php
@@ -2,13 +2,15 @@
 //$team_id = $_POST['name']
 
 //クライアントに返す検索結果はこいつに入れます
-$response = array();
+//$response = array();
 
 
-$row = "test"
-array_push($response, $row);
+//$row = "test"
+//array_push($response, $row);
 
 
 //JSON形式で値を返します
-echo(json_encode($response));
+//echo(json_encode($response));
+
+echo "NULL";
 ?>
