dbd6e16b061c54817dcd16ba7ad585e5fc4254b9
sanada43 <sakurada@oce.co.jp>
2019-02-27 14:40:56 +0900

    action-00

--- upload.php
+++ upload.php
@@ -11,9 +11,9 @@ $file_save = "./file/" . $_FILES["file_1"]["name"];
 // ファイル移動
 $result = @move_uploaded_file($file_tmp, $file_save);
 if ( $result === true ) {
-    echo $file_save;
+    echo "".$_FILES["file_1"]["name"];
 } else {
-    echo "UPLOAD NG";
+    echo "NULL";
     //echo $file_save;
 }
 
