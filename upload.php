0a82ba04c81cf7111ab5572aa30f92c560285d59
sanada43 <sakurada@oce.co.jp>
2019-02-27 19:54:35 +0900

    python-01

--- upload.php
+++ upload.php
@@ -9,31 +9,32 @@ $file_tmp  = $_FILES["file_1"]["tmp_name"];
 $file_save = "./file/" . $_FILES["file_1"]["name"];
 // ①POSTリクエストによるページ遷移かチェック
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // ②エラーコード2だった場合（HTMLのファイル制限超過）
+    if ($_FILES["file_1"]['error'] === 2) {
+        echo 'ファイルサイズを小さくしてください！';
  
-  // ②エラーコード2だった場合（HTMLのファイル制限超過）
-  if ($_FILES["file_1"]['error'] === 2) {
-    echo 'ファイルサイズを小さくしてください！';
+    // ③サイズが0だった場合（ファイルが空）
+    } elseif ($_FILES["file_1"]['size'] === 0) {
+        echo 'ファイルを選択してください！';
  
-  // ③サイズが0だった場合（ファイルが空）
-  } elseif ($_FILES["file_1"]['size'] === 0) {
-    echo 'ファイルを選択してください！';
+    // ④テキストファイルじゃなかった場合
+    } elseif ($_FILES["file_1"]['type'] !== 'text/plain') {
+        echo 'テキストファイルを選択してください！';
  
-  // ④テキストファイルじゃなかった場合
-  } elseif ($_FILES["file_1"]['type'] !== 'text/plain') {
-    echo 'テキストファイルを選択してください！';
- 
-  // アップロードが成功した場合
-  } elseif ($_FILES["file_1"]['error'] === 0) {
+    // アップロードが成功した場合
+    } elseif ($_FILES["file_1"]['error'] === 0) {
     // アップロードされたファイルに、パスとファイル名を設定して保存
-    $result = @move_uploaded_file($file_tmp, $file_save);
-    if ( $result === true ) {
-        echo "".$_FILES["file_1"]["name"];
-    } else {
-        echo "NULL";
-    //echo $file_save;
+        $result = @move_uploaded_file($file_tmp, $file_save);
+        if ( $result === true ) {
+            echo "".$_FILES["file_1"]["name"];
+        } else {
+            echo "NULL";
+        //echo $file_save;
+        }
+    }else {
+        echo 'アップロードに失敗しました！';
     }
-  }else{
-      echo '不正なアクセスです！';
-  }
-
+}else{
+    echo '不正なアクセスです！';
+}
 ?>
