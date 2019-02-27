8d418d43bad2f55cfbb03aeb04ed4d0035911962
sanada43 <sakurada@oce.co.jp>
2019-02-27 11:07:53 +0900

    upload-04

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
 $file_tmp  = $_FILES["file_1"]["tmp_name"];
 
 // 正式保存先ファイルパス
-$file_save = "D:\home\site\wwwroot\uploadfile\" . $_FILES["file_1"]["name"];
+$file_save = "." . $_FILES["file_1"]["name"];
 
 // ファイル移動
 $result = @move_uploaded_file($file_tmp, $file_save);
@@ -12,4 +12,5 @@ if ( $result === true ) {
 } else {
     echo "UPLOAD NG";
 }
+
 ?>
