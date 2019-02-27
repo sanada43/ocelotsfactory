47bbcf461c9082f4ce1449d56579d505fe942ce9
sanada43 <sakurada@oce.co.jp>
2019-02-27 11:29:22 +0900

    upload-05

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
 $file_tmp  = $_FILES["file_1"]["tmp_name"];
 
 // 正式保存先ファイルパス
-$file_save = "." . $_FILES["file_1"]["name"];
+$file_save = "/home/site/file" . $_FILES["file_1"]["name"];
 
 // ファイル移動
 $result = @move_uploaded_file($file_tmp, $file_save);
