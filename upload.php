d9e6cda9fe490a54e7242211012530199c892425
sanada43 <sakurada@oce.co.jp>
2019-02-27 10:09:34 +0900

    first commit

---
+++ upload.php
@@ -0,0 +1,15 @@
+<?php
+// 一時アップロード先ファイルパス
+$file_tmp  = $_FILES["file_1"]["tmp_name"];
+
+// 正式保存先ファイルパス
+$file_save = "C:\Users\400777\Downloads\template-cds\" . $_FILES["file_1"]["name"];
+
+// ファイル移動
+$result = @move_uploaded_file($file_tmp, $file_save);
+if ( $result === true ) {
+    echo "UPLOAD OK";
+} else {
+    echo "UPLOAD NG";
+}
+?>
