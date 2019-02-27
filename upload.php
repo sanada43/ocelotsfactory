c9eac3c3c448f2973625fed4720b3723372752c8
sanada43 <sakurada@oce.co.jp>
2019-02-27 10:32:35 +0900

    upload-02

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
 $file_tmp  = $_FILES["file_1"]["tmp_name"];
 
 // 正式保存先ファイルパス
-$file_save = "https://ocelotswebsite.azurewebsites.net/uploadfile/" . $_FILES["file_1"]["name"];
+$file_save = "D:\home\site\wwwroot\uploadfile\" . $_FILES["file_1"]["name"];
 
 // ファイル移動
 $result = @move_uploaded_file($file_tmp, $file_save);
