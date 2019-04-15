2232d9aad192d5679be3e460be3b2bc54f403e36
sanada43 <sakurada@oce.co.jp>
2019-04-15 10:25:06 +0900

    clean commit

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ try {
         //$dif = $to - $from;
         //echo date('Y-m-d H:i:s' , strtotime('+1 hour'));
         //echo $replace;
-        if (strtotime($replace) <= date('Y-m-d H:i:s' , strtotime('-1 hour')){
+        if (strtotime($replace) <= date('Y-m-d H:i:s' , strtotime('-1 hour'))){
             echo $blob->getName().": ".$blob->getUrl()."<br />";
             echo "<img src='".$blob->getUrl()."'><br />";
         }
